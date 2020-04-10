a1436b87c30dadd853b4ac7d1b295a07518f8699
Michael Dowling <mdowling@gmail.com>
2020-04-09 18:59:53 -0700

    dependencies and naming conventions.

--- device/device_registration_test.go
+++ device/device_registration_test.go
@@ -2,6 +2,7 @@ package device_test
 
 import (
 	"fmt"
+	"github.com/datumchi/protocol-tests/testutils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"os"
@@ -12,14 +13,14 @@ var _ = Describe("DeviceRegistration", func() {
 
 	Context("Using Valid Device Info", func() {
 
-		var identityServiceHost string = os.Getenv("FAIRX_IDENTITY_SERVICE_HOST")
-		var identityServicePort string = os.Getenv("FAIRX_IDENTITY_SERVICE_PORT")
+		var identityServiceHost string = os.Getenv("DATUMCHI_IDENTITY_SERVICE_HOST")
+		var identityServicePort string = os.Getenv("DATUMCHI_IDENTITY_SERVICE_PORT")
 
 
 		It("Device registers itself with the Identity Service using valid data and authenticates successfully", func(){
 
 			addr := fmt.Sprintf("%s:%s", identityServiceHost, identityServicePort)
-			persona := testutils.EstablishValidStandardHumanPersona(addr, "alpha.fairx.io")
+			persona := testutils.EstablishValidStandardHumanPersona(addr, "alpha.datumchi.com")
 			Expect(len(persona.AuthTokens)).To(Equal(1))
 
 
